a8ef2e93899b0a9c8db4d3960a1dcb72f777c999
inho9979 <inho9979@naver.com>
2024-05-01 13:54:11 +0900

    Calculator Lv2-7(Circle 클래스)

--- sparta_Report/reStart/Calculator.java
+++ sparta_Report/reStart/Calculator.java
@@ -5,12 +5,18 @@ import java.util.List;
 
 public class Calculator {
     /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
-    private List<Integer> resultArray;
-
+    /* static, final 활용 */
+    /* final은 생성후 수정이 필요없는 결과값 배열에 적용, static + final 은 수정하지도 않고, 어떤 한 클래스
+      내부에서만 사용하지도 않는 PI 값에 적용 */
+    private final List<Integer> resultArray;
+    /* 원의 넓이 결과를 저장하는 컬렉션 타입의 필드 선언 및 생성 */
+    private final List<Double> circleResultArray;
+    public static final double PI = 3.14;
     /* 연산 결과를 저장하는 컬렉션 타입 필드가 생성자를 통해 초기화 되도록 변경 */
     /* 생성자 구현 */
-    public Calculator(List<Integer> list){
+    public Calculator(List<Integer> list, List<Double> list2){
         resultArray = list;
+        circleResultArray = list2;
     }
 
     public int calculate(int num1, int num2, char oper) throws CalculatorInputException {
@@ -34,6 +40,8 @@ public class Calculator {
                 result = num1 / num2;
             }
         }
+        // 계산메소드에서 결과값을 바로 array에 담도록
+        resultArray.add(result);
         return result;
     }
 
@@ -43,9 +51,8 @@ public class Calculator {
         return tempList;
     }
 
-    // setter는 깊은복사시 문제발생, 얕은복사로 전환
     public void setResultArray(List<Integer> list){
-        resultArray = list;
+        resultArray.addAll(list);
     }
 
     public void removeResult(){
@@ -55,6 +62,30 @@ public class Calculator {
     public void inquiryResultArray(){
         int count = 1;
         for(int e : resultArray){
+            System.out.println(count + "번째 결과: " + e);
+            count++;
+        }
+    }
+
+    /* 원 관련 */
+    public double calculateCircleArea(double radius) {
+        double result = radius * radius * PI;
+        circleResultArray.add(result);
+        return result;
+    }
+
+    public List<Double> getCircleResultArray(){
+        List<Double> tempList = new ArrayList<>(circleResultArray);
+        return tempList;
+    }
+
+    public void setCircleResultArray(List<Double> list){
+        circleResultArray.addAll(list);
+    }
+
+    public void inquiryCircleResultArray(){
+        int count = 1;
+        for(Double e : circleResultArray){
             System.out.println(count + "번째 결과: " + e);
             count++;
         }
--- sparta_Report/reStart/Main.java
+++ sparta_Report/reStart/Main.java
@@ -9,48 +9,64 @@ public class Main {
 
         /* Calculator 인스턴스 생성 */
         /* 요구사항에 맞게 Calculator 인스턴스 생성 부분 수정 */
-        Calculator calculator = new Calculator(new ArrayList<>());
-        List<Integer> resultArray = calculator.getResultArray();
+        Calculator calculator = new Calculator(new ArrayList<>(), new ArrayList<>());
 
         Scanner sc = new Scanner(System.in);
-
         while (true) {
+            System.out.print("사칙연산 계산을 원하면 1, 원 넓이 계산을 원하면 2를 눌러주세요 :");
+            int flag = sc.nextInt();
 
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            int number1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            int number2 = sc.nextInt();
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-            char oper = sc.next().charAt(0);
+            if (flag == 1) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+                int number1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+                int number2 = sc.nextInt();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+                char oper = sc.next().charAt(0);
 
-            int result = 0;
+                int result = 0;
 
-            /* 위 요구사항에 맞게 소스 코드 수정 */
-            try {
-                result = calculator.calculate(number1, number2, oper);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+                /* 위 요구사항에 맞게 소스 코드 수정 */
+                try {
+                    result = calculator.calculate(number1, number2, oper);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                System.out.println("결과: " + result);
+                /* 배열에서 컬렉션으로 변경됨으로써 변경해야하는 부분 구현 */
+
+                /* 요구사항에 맞게 구현 remove”라는 문자열을 입력받으면 가장 먼저 저장된 결과가 삭제될 수 있도록 구현 */
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                if (sc.next().equals("remove"))
+                    calculator.removeResult();
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                /* 위 요구사항에 맞게 소스 코드 수정  클래스에 저장된 연산 결과들을 조회하는 기능을 가진 메서드를 구현 */
+                if (sc.next().equals("inquiry")) {
+                    calculator.inquiryResultArray();
+                }
+            } else if (flag == 2) {
+                System.out.print("반지름을 입력하세요: ");
+                double radius = sc.nextDouble();
+                double resultRadius;
+
+                resultRadius = calculator.calculateCircleArea(radius);
+
+                System.out.println("결과: " + resultRadius);
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                /* 위 요구사항에 맞게 소스 코드 수정  클래스에 저장된 연산 결과들을 조회하는 기능을 가진 메서드를 구현 */
+                if (sc.next().equals("inquiry")) {
+                    calculator.inquiryCircleResultArray();
+                }
+            } else {
+                System.out.println("잘못된 입력입니다 다시 입력해주세요");
+                sc.nextLine();
+                continue;
             }
-            System.out.println("결과: " + result);
-            /* 배열에서 컬렉션으로 변경됨으로써 변경해야하는 부분 구현 */
-            resultArray.add(result);
-            calculator.setResultArray(resultArray);
-
-            /* 요구사항에 맞게 구현 remove”라는 문자열을 입력받으면 가장 먼저 저장된 결과가 삭제될 수 있도록 구현 */
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if (sc.next().equals("remove"))
-                calculator.removeResult();
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-
-            /* 위 요구사항에 맞게 소스 코드 수정  클래스에 저장된 연산 결과들을 조회하는 기능을 가진 메서드를 구현 */
-            if (sc.next().equals("inquiry")) {
-                calculator.inquiryResultArray();
-            }
-
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             /* exit을 입력 받으면 반복 종료 */
             if (sc.next().equals("exit"))
