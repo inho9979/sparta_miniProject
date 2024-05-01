efa47a88d1d4a0c430fcf46c8b5f4579af189f17
inho9979 <inho9979@naver.com>
2024-05-01 12:07:03 +0900

    Calculator Lv1-3(re)

--- sparta_Report/reStart/Main.java
+++ sparta_Report/reStart/Main.java
@@ -18,13 +18,18 @@ public class Main {
 
         int result = 0;
         /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
-        switch(String.valueOf(oper)){
+        switch (String.valueOf(oper)) {
             case "+" -> result = number1 + number2;
             case "-" -> result = number1 - number2;
             case "*" -> result = number1 * number2;
-            case "/" -> result = number1 / number2;
+            case "/" -> {
+                if (number2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다");
+                    break;
+                }
+                result = number1 / number2;
+            }
         }
-        System.out.println("결과: " + result);
-
+        System.out.println("결과: "+ result);
     }
 }
